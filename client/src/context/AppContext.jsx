bdcc159570ec89e4bb04305a96534f046b29aa84
Sudharsan R <sudhan@example.com>
2026-03-26 22:04:28 +0530

    'client': updated and made the changes in AppContext.jsx, ChatBox.jsx, Message.jsx and App.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -19,7 +19,7 @@ export const AppContextProvider = ({ children }) => {
 
     const fetchUserChats = async () => {
         setChats(dummyChats);
-        setSelectedChat(dummyChats[0]);
+        setSelectedChat(dummyChats[0])
     };
 
     // Apply Theme
