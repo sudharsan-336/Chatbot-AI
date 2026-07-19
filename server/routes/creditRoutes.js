0d04cab3af08b2aba7a720dcde18e2f70a985cb2
sudharsan-336 <sudharsan.r03032006@gmail.com>
2026-07-19 20:20:03 +0530

    server: routes: creditRoutes: Create credit router with plans, purchase, and payment verification endpoints

--- server/routes/creditRoutes.js
+++ server/routes/creditRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPlan, purchasePlan } from '../controller/creditController.js';
+import { getPlan, purchasePlan, verifyPayment } from '../controller/creditController.js';
 import { protect } from '../middlewares/auth.js';
 
 const creditRouter = express.Router(); 
 
 creditRouter.get('/plans', getPlan);
 creditRouter.post('/purchase', protect, purchasePlan);
+creditRouter.post('/verify', protect, verifyPayment);
 
 export default creditRouter;
